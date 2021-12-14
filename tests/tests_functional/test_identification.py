0515441413ea8984e5bccf8c890c5849e71443a0
jbbaillet <jbbaillet.dev@gmail.com>
2021-12-14 12:55:08 +0100

    ajout BASE_DIRE dans settings.travis

--- tests/tests_functional/test_identification.py
+++ tests/tests_functional/test_identification.py
@@ -1,39 +1,43 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
 from django.urls import reverse
 import time
 from pureBeurre.settings.travis import BASE_DIR
 
 
 class TestIdentification(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
     
     def test_register(self):
         # Open the browser with webdrive
-        self.service = Service(executable_path=(BASE_DIR + "/tests/tests_functional/chromedriver"))
-        self.service.start()
-        self.driver = webdriver.Remote(self.service.service_url)
-        self.driver.get(self.live_server_url + reverse("register"))
+        self.selenium.get('%s%s' % (self.live_server_url, '/register/'))
 
-        id_username = self.driver.find_element(By.ID, "id_username")
+        id_username = self.selenium.find_element(By.ID, "id_username")
         id_username.send_keys("user1")
-        id_last_name = self.driver.find_element(By.ID, "id_last_name")
+        id_last_name = self.selenium.find_element(By.ID, "id_last_name")
         id_last_name.send_keys("last_name_user1")
-        id_email = self.driver.find_element(By.ID, "id_email")
+        id_email = self.selenium.find_element(By.ID, "id_email")
         id_email.send_keys("user1@email.com")
-        id_password1 = self.driver.find_element(By.ID, "id_password1")
+        id_password1 = self.selenium.find_element(By.ID, "id_password1")
         id_password1.send_keys("Password1!")
-        id_password2 = self.driver.find_element(By.ID, "id_password2")
+        id_password2 = self.selenium.find_element(By.ID, "id_password2")
         id_password2.send_keys("Password1!")
-        signup = self.driver.find_element(By.ID, "submit_register")
+        signup = self.selenium.find_element(By.ID, "submit_register")
         signup.submit()
 
-        self.assertEqual(self.driver.find_element(By.TAG_NAME, 'h2').text,
+        self.assertEqual(self.selenium.find_element(By.TAG_NAME, 'h2').text,
                          "Créer son espace utilisateur")
-        self.assertEqual(self.driver.current_url, self.live_server_url +
+        self.assertEqual(self.selenium.current_url, self.live_server_url +
                          reverse("register"))
         # close the browser
         time.sleep(3)
