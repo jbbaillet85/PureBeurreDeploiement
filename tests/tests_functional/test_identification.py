2fa6be0bd9a72d543d99ec5fada10a42e9352f13
jbbaillet <jbbaillet.dev@gmail.com>
2021-12-14 16:53:33 +0100

    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))

--- tests/tests_functional/test_identification.py
+++ tests/tests_functional/test_identification.py
@@ -1,22 +1,19 @@
-import os
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from django.urls import reverse
 import time
 
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+
 class TestIdentification(StaticLiveServerTestCase):
     
     def test_register(self):
-        # Open the browser with webdrive
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         path_register = reverse('register')
-        driver.get('%s%s' % (self.live_server_url, path_register))
-
+        driver.get(path_register)
+        # Open the browser with webdrive
         id_username = driver.find_element(By.ID, "id_username")
         id_username.send_keys("user1")
         id_last_name = driver.find_element(By.ID, "id_last_name")
@@ -35,5 +32,5 @@ class TestIdentification(StaticLiveServerTestCase):
         self.assertEqual(driver.current_url, self.live_server_url +
                          reverse("register"))
         # close the browser
-        time.sleep(3)
+        time.sleep(300)
         driver.close()
