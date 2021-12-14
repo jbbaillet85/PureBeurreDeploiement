9401b6f8a40836cca9cc38d40631cb44c1b4124f
jbbaillet <jbbaillet.dev@gmail.com>
2021-12-14 13:52:37 +0100

    path_register

--- tests/tests_functional/test_identification.py
+++ tests/tests_functional/test_identification.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from django.urls import reverse
 import time
 
